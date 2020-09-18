b57356e1c4b6194bb9062c10ced888cab654ef7d
Lucas Diniz <kbelodiniz@hotmail.com>
2020-09-18 17:16:04 +0100

    [PI] Resolved conflicts from master Branch
    * Added a setup.py for a local virtualenv giving better IDE introspection

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='prospects_insight',
+    version='0.1.0',
+    author='Luke',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=open('requirements.txt').readlines(),
+    setup_requires=[],
+    tests_require=[],
+)
