3164ce4c68ec3146d359e1257603af5431d42f27
Lucas Diniz <kbelodiniz@hotmail.com>
2020-09-28 15:57:27 +0100

    [PI] WIP: Add templates to django

--- insight/prospect/views.py
+++ insight/prospect/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = {"test": "This is a test context"}
     return render(request, 'prospect/index.html', context)
+
+
+def report(request):
+    context = {"test": "This is a test context"}
+    return render(request, 'prospect/report.html', context)
