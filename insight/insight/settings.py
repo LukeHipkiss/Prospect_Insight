7f3b138a5bdcf383878136fe0993a5236c5e3174
Lucas Diniz <kbelodiniz@hotmail.com>
2020-10-19 16:39:10 +0100

    [PI] Adjusting setting for prod deployment

--- insight/insight/settings.py
+++ insight/insight/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
