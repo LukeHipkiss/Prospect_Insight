dbbd3ea7615d0c3c803c444619db35d9da4e1f3f
Lucas Diniz <kbelodiniz@hotmail.com>
2020-10-24 22:07:26 +0100

    [PI] Register models for the admin page

--- insight/prospect/admin.py
+++ insight/prospect/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin  # noqa: F401
 
+from .models import Report, Prospect
 # Register your models here.
+
+
+class ReportAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProspectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Report, ReportAdmin)
+admin.site.register(Prospect, ProspectAdmin)
